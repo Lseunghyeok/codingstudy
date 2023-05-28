07b73ab7d64baeca63b5ea4d8334b5d18cbefdbf
Lseunghyeok <seunghyeok031109@gmail.com>
2023-05-29 02:47:10 +0900

    백준 25314번

---
+++ BK25314.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class BK25314 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            int num = sc.nextInt();
+            int div = num/4;
+            for(int i=0;i<div;i++) {
+                System.out.print("long ");
+            }
+            System.out.print("int");
+	}
+}
