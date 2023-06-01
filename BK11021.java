e391dccf483bf3c935d70e2ca8ffb1d5a432bcf1
Lseunghyeok <seunghyeok031109@gmail.com>
2023-06-02 02:51:31 +0900

    백준 11021 반복문

---
+++ BK11021.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class BK11021 {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int cnt = sc.nextInt();
+		for(int i=0; i<cnt; i++) {
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+			System.out.println("Case #" + (i+1) + ": " + (num1+num2));
+		}
+		}
+	}
